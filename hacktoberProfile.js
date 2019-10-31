5fa9ac0a1dde3a5065ffc4d150ae3b0b238400cf
anojiniA <32506321+anojiniA@users.noreply.github.com>
2019-10-31 19:01:17 +0530

    Add my profile

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+  {
+    name : "Anojini",
+    place : "Colombo, Sri Lanka",
+    github : "anojiniA"
   }
 ];
